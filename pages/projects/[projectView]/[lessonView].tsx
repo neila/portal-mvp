4a4eb7e1d8ae9f08aafcfb260ee5489c45e36629
cohurin <neila@users.noreply.github.com>
2022-04-12 21:28:24 +0900

    fix again

--- pages/projects/[projectView]/[lessonView].tsx
+++ pages/projects/[projectView]/[lessonView].tsx
@@ -70,7 +70,7 @@ export default function lesson({
 
                     {/* back to portal */}
                     <Link href={`/projects/${encodeURIComponent(projectView)}/ `} >
-                        <div className="absolute inset-y-0 left-1/2 -translate-x-1/2">
+                        <div className="absolute inset-y-0 right-1/2 translate-x-1/2">
                             <div className="bg-neutral-300 border-2 px-4 py-2 rounded-full cursor-pointer">
                                 <a className="text-neutral-600 text-base laptop:text-lg font-bold">
                                     Back to project overview
